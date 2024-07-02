1f751a6a1627f19c8a3d3bb9d04a9849b071f7ab
dmcanete <canetediegomartin@gmail.com>
2024-07-02 18:41:35 -0300

    Conectando base de datos

--- src/uploads/app.py
+++ src/uploads/app.py
@@ -5,6 +5,14 @@ from flaskext.mysql import MySQL
 app = Flask(__name__)
 mysql = MySQL()
 
+app.config['MYSQL_DATA_BASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'arata2165'
+app.config['MYSQL_DATABASE_BS'] = 'clientes'
+
+mysql.init_app(app)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
     
